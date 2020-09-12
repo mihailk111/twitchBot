32600a81b4d70bf6d595ca79fd072f34259358ff
M <mhailk2048@gmail.com>
2020-09-12 23:04:28 +0500

    just a commit

--- gitsend.js
+++ gitsend.js
@@ -4,16 +4,6 @@ const  {
 }= require('child_process');
 
 
-// exec('git add *', () => {
-
-//     exec('git commit -m "just a commit" ', () => {
-
-//         exec('git push', () => {
-
-//         });
-//     });
-// });
-
 execSync ('git add *');
 execSync('git commit -m "just a commit" ');
 execSync('git push');
